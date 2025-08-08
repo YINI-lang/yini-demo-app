35e405798b485ccd86aab61857b5d274e2e84490
Marko K. S. <marko.seppanen@gmail.com>
2025-08-09 01:29:31 +0200

    Added start of  example medium

---
+++ js-cjs/medium/index.js
@@ -0,0 +1,32 @@
+/*
+    This script loads and parses a config.yini
+    file then prints its contents using CommonJS.
+*/
+
+const path = require('path');
+const YINI = require('yini-parser');
+
+// Path to your config file.
+const file = path.join(__dirname, 'config.yini');
+
+try {
+    // Parse the YINI config file.
+    const config = YINI.parseFile(file);
+
+    // If you want to parse the file in strict mode, use this line instead:
+    // const config = YINI.parseFile(file, true);
+
+    // Print some value in the config.
+    console.log('App Name = '+config.App.name)
+    console.log('DB Host = '+config.App.Database.host)
+    console.log()
+
+    // Print the result.
+    console.log('Parsed config:');
+    console.log(config)
+
+    console.log('Parsed config as JSON:');
+    console.log(JSON.stringify(config, null, 2));
+} catch (error) {
+    console.error('Error parsing config.yini:', error.message);
+}
